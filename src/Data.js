e43b64bdb74514060f62b9e0cad2c0e4ba93d60d
Vasile <vasile.taranovici@gmail.com>
2019-12-18 17:14:54 +0200

    ♻️  some changes

--- src/Data.js
+++ src/Data.js
@@ -1,91 +1,95 @@
-import Minimongo from '@xvonabur/minimongo-cache';
-import Trackr from 'trackr';
-import isReactNative from './isReactNative';
-
-let InteractionManager;
-let ReactNative;
-
-if (isReactNative) {
-    InteractionManager = require('react-native').InteractionManager; // eslint-disable-line
-    ReactNative = require('react-native/Libraries/Renderer/shims/ReactNative'); // eslint-disable-line
-}
+import Minimongo from "@xvonabur/minimongo-cache";
+import Trackr from "trackr";
+import { InteractionManager } from "react-native";
+import ReactNative from "react-native/Libraries/Renderer/shims/ReactNative";
 
 process.nextTick = setImmediate;
 
 const db = new Minimongo();
 db.debug = false;
-db.batchedUpdates = ReactNative ? ReactNative.unstable_batchedUpdates : undefined;
+db.batchedUpdates = ReactNative
+  ? ReactNative.unstable_batchedUpdates
+  : undefined;
 
 function runAfterOtherComputations(fn) {
-    InteractionManager ? InteractionManager.runAfterInteractions(() => {
+  InteractionManager
+    ? InteractionManager.runAfterInteractions(() => {
         Trackr.afterFlush(() => {
-            fn();
+          fn();
         });
-    }) : Trackr.afterFlush(() => {
+      })
+    : Trackr.afterFlush(() => {
         fn();
-    });
+      });
 }
 
 export default {
-    _endpoint: null,
-    _options: null,
-    ddp: null,
-    subscriptions: {},
-    db,
-    calls: [],
+  _endpoint: null,
+  _options: null,
+  ddp: null,
+  subscriptions: {},
+  db,
+  calls: [],
 
-    getUrl() {
-        return this._endpoint.substring(0, this._endpoint.indexOf('/websocket'));
-    },
+  getUrl() {
+    return this._endpoint.substring(0, this._endpoint.indexOf("/websocket"));
+  },
 
-    waitDdpReady(cb) {
-        if (this.ddp) {
-            cb();
-        } else {
-            runAfterOtherComputations(() => {
-                this.waitDdpReady(cb);
-            });
-        }
-    },
+  waitDdpReady(cb) {
+    if (this.ddp) {
+      cb();
+    } else {
+      runAfterOtherComputations(() => {
+        this.waitDdpReady(cb);
+      });
+    }
+  },
 
-    _cbs: [],
-    onChange(cb) {
-        this.db.on('change', cb);
-        this.ddp.on('connected', cb);
-        this.ddp.on('disconnected', cb);
-        this.on('loggingIn', cb);
-        this.on('change', cb);
-    },
-    offChange(cb) {
-        this.db.off('change', cb);
-        this.ddp.off('connected', cb);
-        this.ddp.off('disconnected', cb);
-        this.off('loggingIn', cb);
-        this.off('change', cb);
-    },
-    on(eventName, cb) {
-        this._cbs.push({
-            eventName,
-            callback: cb,
-        });
-    },
-    off(eventName, cb) {
-        this._cbs.splice(this._cbs.findIndex(_cb => _cb.callback === cb && _cb.eventName === eventName), 1);
-    },
-    notify(eventName) {
-        this._cbs.forEach((cb) => {
-            if (cb.eventName === eventName && typeof cb.callback === 'function') {
-                cb.callback();
-            }
-        });
-    },
-    waitDdpConnected(cb) {
-        if (this.ddp && this.ddp.status === 'connected') {
-            cb();
-        } else if (this.ddp) {
-            this.ddp.once('connected', cb);
-        } else {
-            setTimeout(() => { this.waitDdpConnected(cb); }, 10);
-        }
-    },
+  _cbs: [],
+  onChange(cb) {
+    this.db.on("change", cb);
+    this.ddp.on("connected", cb);
+    this.ddp.on("disconnected", cb);
+    this.on("loggingIn", cb);
+    this.on("change", cb);
+  },
+  offChange(cb) {
+    this.db.off("change", cb);
+    this.ddp.off("connected", cb);
+    this.ddp.off("disconnected", cb);
+    this.off("loggingIn", cb);
+    this.off("change", cb);
+  },
+  on(eventName, cb) {
+    this._cbs.push({
+      eventName,
+      callback: cb
+    });
+  },
+  off(eventName, cb) {
+    this._cbs.splice(
+      this._cbs.findIndex(
+        _cb => _cb.callback === cb && _cb.eventName === eventName
+      ),
+      1
+    );
+  },
+  notify(eventName) {
+    this._cbs.forEach(cb => {
+      if (cb.eventName === eventName && typeof cb.callback === "function") {
+        cb.callback();
+      }
+    });
+  },
+  waitDdpConnected(cb) {
+    if (this.ddp && this.ddp.status === "connected") {
+      cb();
+    } else if (this.ddp) {
+      this.ddp.once("connected", cb);
+    } else {
+      setTimeout(() => {
+        this.waitDdpConnected(cb);
+      }, 10);
+    }
+  }
 };
--- src/user/User.js
+++ src/user/User.js
@@ -1,134 +1,134 @@
-import Data from '../Data';
-import { hashPassword } from '../../lib/utils';
-import call from '../Call';
-import { Collection } from '../Collection';
-import isReactNative from '../isReactNative';
+import Data from "../Data";
+import { hashPassword } from "../../lib/utils";
+import call from "../Call";
+import { Collection } from "../Collection";
+import AsyncStorage from "@react-native-community/async-storage";
 
-let Storage;
-
-if (isReactNative) {
-    // eslint-disable-next-line
-    Storage = require('react-native').AsyncStorage;
-} else {
-    Storage = localStorage;
-}
-
-const TOKEN_KEY = 'reactnativemeteor_usertoken';
+const TOKEN_KEY = "reactnativemeteor_usertoken";
 
 module.exports = {
-    user() {
-        if (!this._userIdSaved) return null;
+  user() {
+    if (!this._userIdSaved) return null;
 
-        return this.users.findOne(this._userIdSaved);
-    },
-    users: new Collection('users'),
-    userId() {
-        if (!this._userIdSaved) return null;
+    return this.users.findOne(this._userIdSaved);
+  },
+  users: new Collection("users"),
+  userId() {
+    if (!this._userIdSaved) return null;
 
-        const user = this.users.findOne(this._userIdSaved);
-        return user && user._id;
-    },
-    _isLoggingIn: true,
-    loggingIn() {
-        return this._isLoggingIn;
-    },
-    logout(callback) {
-        call('logout', (err) => {
-            this.handleLogout();
-            this.connect();
+    const user = this.users.findOne(this._userIdSaved);
+    return user && user._id;
+  },
+  _isLoggingIn: true,
+  loggingIn() {
+    return this._isLoggingIn;
+  },
+  logout(callback) {
+    call("logout", err => {
+      this.handleLogout();
+      this.connect();
 
-            typeof callback === 'function' && callback(err);
-        });
-    },
-    handleLogout() {
-        Storage.removeItem(TOKEN_KEY);
-        Data._tokenIdSaved = null;
-        this._userIdSaved = null;
-    },
-    loginWithPassword(selector, password, callback) {
-        if (typeof selector === 'string') {
-            if (selector.indexOf('@') === -1) { selector = { username: selector }; } else { selector = { email: selector }; }
-        }
+      typeof callback === "function" && callback(err);
+    });
+  },
+  async handleLogout() {
+    await AsyncStorage.removeItem(TOKEN_KEY);
+    Data._tokenIdSaved = null;
+    this._userIdSaved = null;
+  },
+  loginWithPassword(selector, password, callback) {
+    if (typeof selector === "string") {
+      if (selector.indexOf("@") === -1) {
+        selector = { username: selector };
+      } else {
+        selector = { email: selector };
+      }
+    }
 
-        this._startLoggingIn();
-        call('login', {
-            user: selector,
-            password: hashPassword(password),
-        }, (err, result) => {
-            this._endLoggingIn();
+    this._startLoggingIn();
+    call(
+      "login",
+      {
+        user: selector,
+        password: hashPassword(password)
+      },
+      (err, result) => {
+        this._endLoggingIn();
 
-            this._handleLoginCallback(err, result);
+        this._handleLoginCallback(err, result);
 
-            typeof callback === 'function' && callback(err);
-        });
-    },
-    logoutOtherClients(callback = () => {}) {
-        call('getNewToken', (err, res) => {
-            if (err) return callback(err);
+        typeof callback === "function" && callback(err);
+      }
+    );
+  },
+  logoutOtherClients(callback = () => {}) {
+    call("getNewToken", (err, res) => {
+      if (err) return callback(err);
 
-            this._handleLoginCallback(err, res);
+      this._handleLoginCallback(err, res);
 
-            call('removeOtherTokens', (err) => {
-                callback(err);
-            });
-        });
-    },
-    _login(user, callback) {
-        this._startLoggingIn();
-        this.call('login', user, (err, result) => {
-            this._endLoggingIn();
+      call("removeOtherTokens", err => {
+        callback(err);
+      });
+    });
+  },
+  _login(user, callback) {
+    this._startLoggingIn();
+    this.call("login", user, (err, result) => {
+      this._endLoggingIn();
 
-            this._handleLoginCallback(err, result);
+      this._handleLoginCallback(err, result);
 
-            typeof callback === 'function' && callback(err);
-        });
-    },
-    _startLoggingIn() {
-        this._isLoggingIn = true;
-        Data.notify('loggingIn');
-    },
-    _endLoggingIn() {
-        this._isLoggingIn = false;
-        Data.notify('loggingIn');
-    },
-    _handleLoginCallback(err, result) {
-        if (!err) { // save user id and token
-            Storage.setItem(TOKEN_KEY, result.token);
-            Data._tokenIdSaved = result.token;
-            this._userIdSaved = result.id;
-            Data.notify('onLogin');
-        } else {
-            Data.notify('onLoginFailure');
-            this.handleLogout();
-        }
-        Data.notify('change');
-    },
-    _loginWithToken(value) {
-        Data._tokenIdSaved = value;
-        if (value !== null) {
-            this._startLoggingIn();
-            call('login', { resume: value }, (err, result) => {
-                this._endLoggingIn();
-                this._handleLoginCallback(err, result);
-            });
-        } else {
-            this._endLoggingIn();
-        }
-    },
-    getAuthToken() {
-        return Data._tokenIdSaved;
-    },
-    getAuthTokenFromStorage() {
-        return Storage.getItem(TOKEN_KEY);
-    },
-    async _loadInitialUser() {
-        let value = null;
-        try {
-            value = await this.getAuthTokenFromStorage();
-        } catch (error) {
-            console && console.warn(`Error Loading User: ${error.message}`);
-        } finally {
-            this._loginWithToken(value);
-        }
-    },
+      typeof callback === "function" && callback(err);
+    });
+  },
+  _startLoggingIn() {
+    this._isLoggingIn = true;
+    Data.notify("loggingIn");
+  },
+  _endLoggingIn() {
+    this._isLoggingIn = false;
+    Data.notify("loggingIn");
+  },
+  async _handleLoginCallback(err, result) {
+    if (!err) {
+      // save user id and token
+      await AsyncStorage.setItem(TOKEN_KEY, result.token);
+      Data._tokenIdSaved = result.token;
+      this._userIdSaved = result.id;
+      Data.notify("onLogin");
+    } else {
+      Data.notify("onLoginFailure");
+      this.handleLogout();
+    }
+    Data.notify("change");
+  },
+  _loginWithToken(value) {
+    Data._tokenIdSaved = value;
+    if (value !== null) {
+      this._startLoggingIn();
+      call("login", { resume: value }, (err, result) => {
+        this._endLoggingIn();
+        this._handleLoginCallback(err, result);
+      });
+    } else {
+      this._endLoggingIn();
+    }
+  },
+  getAuthToken() {
+    return Data._tokenIdSaved;
+  },
+  async getAuthTokenFromStorage() {
+    return AsyncStorage.getItem(TOKEN_KEY);
+  },
+  async _loadInitialUser() {
+    let value = null;
+    try {
+      value = await this.getAuthTokenFromStorage();
+    } catch (error) {
+      console && console.warn(`Error Loading User: ${error.message}`);
+    } finally {
+      this._loginWithToken(value);
+    }
+  }
 };
